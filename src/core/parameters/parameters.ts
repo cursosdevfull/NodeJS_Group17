6730c917716f5047ef914d12bd0fd4f6c3fe1267
Sergio Hidalgo <shidalgoc@auna.pe>
2024-06-15 13:22:15 -0500

    fix parameters

--- src/core/parameters/parameters.ts
+++ src/core/parameters/parameters.ts
@@ -4,11 +4,11 @@ import { UserEntity } from "../../modules/user/infrastructure/entities/user.enti
 
 export class Parameters {
   static get port(): number {
-    return process.env.port ? Number(process.env.port) : 3000;
+    return process.env.PORT ? Number(process.env.port) : 3000;
   }
 
   static get host(): string {
-    return process.env.host || "0.0.0.0";
+    return process.env.HOST || "0.0.0.0";
   }
 
   static get dbConfig() {
